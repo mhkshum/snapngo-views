8896bb46696f0d5188685ee65f9c13bb777c6502
Megan Shum <meganshum@Megans-MacBook-Air.local>
2017-03-14 13:04:02 -0400

    Initial Commit

---
+++ SnapnGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapnGo
+//
+//  Created by Megan Shum on 3/14/17.
+//  Copyright © 2017 Megan Shum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
